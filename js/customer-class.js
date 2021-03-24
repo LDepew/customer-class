a93fea6a625746b55971d2bf3f61f35b7b0f0ac8
Lance Depew <Metal_Death_Music@yahoo.com>
2021-03-24 14:32:54 -0400

    add constructor to js

---
+++ js/customer-class.js
@@ -0,0 +1,9 @@
+class Customer {
+
+    constructor(id, name, stateCode, sales) {
+        this.id = id;
+        this.name = name;
+        this.stateCode = stateCode;
+        this.sales = sales;
+    }
+}
